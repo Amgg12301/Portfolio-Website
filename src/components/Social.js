90aea8fea136d700b2f858e408d7bd646553923f
Amogh Giri <amoghgiri01@gmail.com>
2020-09-07 01:11:27 -0400

    small tweaks to home

--- src/components/Social.js
+++ src/components/Social.js
@@ -6,11 +6,11 @@ class Social extends Component{
         return (
             <div className="socials">
                 <h3 align="center">Let's Stay Connected!</h3>
-                <SocialIcon url="http://linkedin.com/in/agiri01" style={{ height: 40, width: 40, margin: "0px 5px 5px 5px"}}/>
-                <SocialIcon url="https://github.com/Amgg12301" bgColor="black" style={{ height: 40, width: 40, margin: "0px 5px 5px 5px"}}/>
-                <SocialIcon url="mailto:amoghgiri01@gmail.com" bgColor="#F14C0E" style={{ height: 40, width: 40, margin: "0px 5px 5px 5px"}}/>
-                <SocialIcon url="https://www.facebook.com/amgg12301/" style={{ height: 40, width: 40, margin: "0px 5px 5px 5px"}}/>
-                <SocialIcon url="https://www.instagram.com/amogh_giri01/" bgColor="#FF33FF" style={{ height: 40, width: 40, margin: "0px 5px 5px 5px"}}/>
+                <SocialIcon url="http://linkedin.com/in/agiri01" style={{ height: 35, width: 35, margin: "0px 5px 5px 5px"}}/>
+                <SocialIcon url="https://github.com/Amgg12301" bgColor="black" style={{ height: 35, width: 35, margin: "0px 5px 5px 5px"}}/>
+                <SocialIcon url="mailto:amoghgiri01@gmail.com" bgColor="#F14C0E" style={{ height: 35, width: 35, margin: "0px 5px 5px 5px"}}/>
+                <SocialIcon url="https://www.facebook.com/amgg12301/" style={{ height: 35, width: 35, margin: "0px 5px 5px 5px"}}/>
+                <SocialIcon url="https://www.instagram.com/amogh_giri01/" bgColor="#FF33FF" style={{ height: 35, width: 35, margin: "0px 5px 5px 5px"}}/>
             </div>
         )
     }
