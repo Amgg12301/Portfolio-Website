690209757bbff13aaa05827a8d381e6f802667a7
Amogh Giri <amoghgiri01@gmail.com>
2020-09-07 15:01:33 -0400

    decided to use cards

--- src/contents/About.js
+++ src/contents/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Social from '../components/Social';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 class About extends Component{
     render() {
@@ -7,6 +9,50 @@ class About extends Component{
             <div className="about">
                 <div className="content">
                     <h1>Quick Facts About Me...</h1>
+                    <div className="card1">
+                    <CardGroup style={{display: 'flex', flexDirection: 'row'}}>
+                    <Card style={{flex: 1}}>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card style={{flex: 1}}>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                            content.{' '}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card style={{flex: 1}}>
+                        <Card.Img variant="top" src="holder.js/100px160" />
+                        <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    </CardGroup>
+                    </div>
                 </div>
                 <Social />
             </div>
