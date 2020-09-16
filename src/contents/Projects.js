6eb60117745d14daf5f254f24b423e0e74dab721
Amogh Giri <amoghgiri01@gmail.com>
2020-09-16 12:36:04 -0400

    fixed projects section

--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -7,11 +7,14 @@ class Skills extends Component{
             <div className="skills">
                 <div className="content_skills">
                     <h1><b>Let's take a look at what I've built...</b></h1>
-                    <div class="card1">
+                    <div class="project_card">
                         <h3>Have you seen my 3rd-place winning Capital One Hackathon project?</h3>
                         <h3>More information about this project and my other projects on my Github!</h3>
                         <h4>Click me!! :)</h4>
-                        <SocialIcon url="https://github.com/Amgg12301" bgColor="black" style={{ height: 70, width: 70, margin: "0px 5px 5px 5px" }}/>
+                        <div class="github_icon">
+                            <SocialIcon url="https://github.com/Amgg12301" bgColor="black" style={{ height: 50, width: 50 }}/>
+                        </div>
+                        <br></br>
                     </div>
                 </div>
             </div>
