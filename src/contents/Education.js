1c20bc17565b61c42cd1e6f431579b319cce7d8b
Amogh Giri <amoghgiri01@gmail.com>
2020-09-16 01:37:48 -0400

    projects + edu temp done

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import umd_logo from '../umd_logo.jpg';
 
 class Education extends Component{
     render() {
@@ -7,7 +8,9 @@ class Education extends Component{
                 <div className="content_edu">
                     <h1><b>Where am I studying at right now?</b></h1>
                     <div class="card1">
-                        <h2>Work in progress, sorry about that :)</h2>
+                        <img src={umd_logo} alt="profile" className="logo"></img>
+                        <h2>University of Maryland, College Park</h2>
+                        <h3>Go Terps!!</h3>
                     </div>
                 </div>
             </div>
--- src/contents/Projects.js
+++ src/contents/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 class Skills extends Component{
     render() {
@@ -7,7 +8,10 @@ class Skills extends Component{
                 <div className="content_skills">
                     <h1><b>Let's take a look at what I've built...</b></h1>
                     <div class="card1">
-                        <h2>Work in progress, sorry about that :)</h2>
+                        <h3>Have you seen my 3rd-place winning Capital One Hackathon project?</h3>
+                        <h3>More information about this project and my other projects on my Github!</h3>
+                        <h4>Click me!! :)</h4>
+                        <SocialIcon url="https://github.com/Amgg12301" bgColor="black" style={{ height: 70, width: 70, margin: "0px 5px 5px 5px" }}/>
                     </div>
                 </div>
             </div>
